9bc6fe8a015590cc8f6b4d88a2312a771cc5317f
sai <tlsai@gmail.com>
2017-04-30 17:30:19 +0530

    fdf

--- TestGitProj/src/com/git/pkg/Test.java
+++ TestGitProj/src/com/git/pkg/Test.java
@@ -3,11 +3,8 @@ package com.git.pkg;
 public class Test {
 
 	public static void main(String[] args) {
-	System.out.println("hello");
+	System.out.println("sairam");
 
-	System.out.println("helgfglo");
-
-	System.out.println("herrrllo");
 
 
 	}
